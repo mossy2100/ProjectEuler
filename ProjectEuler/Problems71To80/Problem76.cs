7f6856c7f6e15fd4697bdc332e53b9e59d58de19
Shaun Moss <shaun@astromultimedia.com>
2022-12-14 04:13:40 +1000

    Completed Problem 73. Added empty problem classes for problems 73-80.

---
+++ ProjectEuler/Problems71To80/Problem76.cs
@@ -0,0 +1,13 @@
+namespace AstroMultimedia.ProjectEuler;
+
+/// <summary>
+///
+/// <see href="https://projecteuler.net/problem=76" />
+/// </summary>
+public static class Problem76
+{
+    public static long Answer()
+    {
+        return 0;
+    }
+}
---
+++ ProjectEuler/Problems71To80/Problem78.cs
@@ -0,0 +1,13 @@
+namespace AstroMultimedia.ProjectEuler;
+
+/// <summary>
+///
+/// <see href="https://projecteuler.net/problem=78" />
+/// </summary>
+public static class Problem78
+{
+    public static long Answer()
+    {
+        return 0;
+    }
+}
