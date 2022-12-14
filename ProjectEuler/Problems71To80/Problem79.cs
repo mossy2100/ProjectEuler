64001eefc151926933da941d135568288a73dc69
Shaun Moss <shaun@astromultimedia.com>
2022-12-15 07:06:42 +1000

    Fixed conflict.

--- ProjectEuler/Problems71To80/Problem79.cs
+++ ProjectEuler/Problems71To80/Problem79.cs
@@ -2,15 +2,9 @@ namespace AstroMultimedia.ProjectEuler;
 
 /// <summary>
 ///
-<<<<<<< HEAD:ProjectEuler/Problems71To80/Problem79.cs
 /// <see href="https://projecteuler.net/problem=79" />
 /// </summary>
 public static class Problem79
-=======
-/// <see href="https://projecteuler.net/problem=74" />
-/// </summary>
-public static class Problem74
->>>>>>> origin/main:ProjectEuler/Problems71To80/Problem74.cs
 {
     public static long Answer()
     {
