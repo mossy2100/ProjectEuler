6b8405271b6a0ef5eef64e1ffa25b3f7bd739b47
Shaun Moss <shaun@astromultimedia.com>
2022-12-16 04:26:01 +1000

    Tightened up the solution to Problem 75 by removing the PythagoreanTriple class.

--- ProjectEuler/Problems71To80/Problem75.cs
+++ ProjectEuler/Problems71To80/Problem75.cs
@@ -8,73 +8,11 @@ public static class Problem75
 {
     public static long Answer()
     {
-        ulong max = 1_500_000;
-        int[] count = new int[max + 1];
-        Dictionary<ulong, ulong> squares = PolygonalNumbers.GetAllSquareUpTo(max * max);
-        for (ulong len = 12; len <= max; len++)
-        {
-            ulong maxA = (len - 3) / 3;
-            for (ulong a = 1; a <= maxA; a++)
-            {
-                // Make b > a, otherwise we'll find duplicates (e.g. 4, 3, 5 as well as 3, 4, 5).
-                ulong maxB = (len - a - 1) / 2;
-                for (ulong b = a + 1; b <= maxB; b++)
-                {
-                    if (len < 2 * b + a + 1)
-                    {
-                        break;
-                    }
-
-                    // Calculate the candidate hypotenuse.
-                    ulong c = len - a - b;
-
-                    // Test the values, without using Pow() or division.
-                    ulong aSqrPlusBSqr = squares[a] + squares[b];
-                    ulong cSqr = squares[c];
-
-                    if (aSqrPlusBSqr > cSqr)
-                    {
-                        // Stop searching.
-                        break;
-                    }
-
-                    if (aSqrPlusBSqr == cSqr)
-                    {
-                        // Found a solution.
-                        // Console.WriteLine($"{a}^2 + {b}^2 = {c}^2");
-                        count[len]++;
-                    }
-                } // for b
-            } // for a
-
-            if (len % 1000 == 0)
-            {
-                Console.WriteLine($"len = {len}");
-            }
-
-        } // for len
-
-        return count.Count(i => i == 1);
-    }
-
-    public static Dictionary<ulong, ulong> Squares = new ();
-
-    public static ulong Sqr(ulong n)
-    {
-        if (!Squares.ContainsKey(n))
-        {
-            Squares[n] = n * n;
-        }
-        return Squares[n];
-    }
-
-    public static long Answer2()
-    {
         // Set the maximum perimeter.
         const ulong max = 1_500_000;
 
         // Keep track of how many solutions we find, grouped by perimeter.
-        List<PythagoreanTriple> triples = new ();
+        List<(ulong a, ulong b, ulong c)> triples = new ();
 
         // Also keep track of how many solutions we find for each length.
         int[] count = new int[max + 1];
@@ -99,9 +37,11 @@ public static class Problem75
                 }
 
                 // Calculate a, b, c, and len.
-                ulong a = Sqr(m) - Sqr(n);
+                ulong mSqr = m * m;
+                ulong nSqr = n * n;
+                ulong a = mSqr - nSqr;
                 ulong b = 2 * m * n;
-                ulong c = Sqr(m) + Sqr(n);
+                ulong c = mSqr + nSqr;
                 ulong len = a + b + c;
 
                 // If perimeter is too large, stop checking values for m.
@@ -117,7 +57,7 @@ public static class Problem75
                 }
 
                 // Check we haven't found it already.
-                PythagoreanTriple triple = new (a, b, c);
+                (ulong a, ulong b, ulong c) triple = (a, b, c);
                 if (!triples.Contains(triple))
                 {
                     // Found a primitive triple.
@@ -135,61 +75,15 @@ public static class Problem75
         }
 
         // Now we have the primitives, count up the multiples.
-        foreach (PythagoreanTriple triple in triples)
+        foreach ((ulong a, ulong b, ulong c) triple in triples)
         {
-            for (ulong k = 2; k <= max / triple.Length; k++)
+            ulong len = triple.a + triple.b + triple.c;
+            for (ulong k = 2; k <= max / len; k++)
             {
-                count[k * triple.Length]++;
+                count[k * len]++;
             }
         }
 
-        // string output = string.Join(" ", triples
-        //     .Where(t => t.IsPrimitive)
-        //     .OrderBy(t => t.Length)
-        //     .Select(t => t.ToString()));
-        // Console.WriteLine(output);
-
         return count.Count(i => i == 1);
     }
-}
-
-public class PythagoreanTriple
-{
-    public ulong a;
-    public ulong b;
-    public ulong c;
-
-    public PythagoreanTriple(ulong a, ulong b, ulong c)
-    {
-        if (a < b)
-        {
-            this.a = a;
-            this.b = b;
-        }
-        else
-        {
-            this.a = b;
-            this.b = a;
-        }
-        this.c = c;
-    }
-
-    public ulong Length => a + b + c;
-
-    public bool IsPrimitive => Primes.AreCoprime(a, b);
-
-    public override string ToString() =>
-        $"({a}, {b}, {c})";
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is not PythagoreanTriple triple)
-        {
-            return false;
-        }
-        return triple.a == a && triple.b == b && triple.c == c;
-    }
-
-    public override int GetHashCode() =>
-        ToString().GetHashCode();
 }
--- ProjectEulerTests/Test71To80.cs
+++ ProjectEulerTests/Test71To80.cs
@@ -16,7 +16,7 @@ public class Test71To80
     public void TestProblem74() => Assert.AreEqual(402, Problem74.Answer());
 
     [TestMethod]
-    public void TestProblem75() => Assert.AreEqual(161667, Problem75.Answer2());
+    public void TestProblem75() => Assert.AreEqual(161667, Problem75.Answer());
 
     // [TestMethod]
     // public void TestProblem76() => Assert.AreEqual(661, Problem76.Answer());
